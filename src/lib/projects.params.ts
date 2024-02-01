de2225eacdea760ed7f5b986c0e945fe5f79c4b6
KAUSSHIK <kausshikmanojkumar@gmail.com>
2024-01-31 23:36:51 -0600

    Implement bug fix for issue #123

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -15,78 +15,53 @@ const MY_PROJECTS: Array<Project> = [
 		name: 'SwipeHire: A new way to hire',
 		period: {
 			from: new Date('2023-01-16'),
-			to: new Date('2023-05-16')
+			to: new Date('2023-05-17')
 		},
 		skills: getSkills('java', 'Spring', 'mysql', 'gitlab'),
 		type: 'Android Application',
-		screenshots: [
-			{
-				label: 'screen 1',
-				src: '/Users/kausshik/WebPortfolio/Files/img1.png'
-			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			}
-		]
 	},
 	{
 		slug: 'brain-tumor-classification',
 		color: '#f27f1b',
 		description:
-			'A Deep Learning model to classify brain tumors from 2D MRI scans comprising of over 3500 images.',
+			'This Brain Tumor Classification project was deeply personal, driven by my motivation to contribute to the medical field following my mother\'s recovery from a brain tumor.<div style="height: 10px;"></div>Utilizing a dataset of over 3,000 MRI images, I developed a convolutional neural network (CNN) using TensorFlow and Keras.<div style="height: 10px;"></div>By integrating transfer learning with EfficientNet, I enhanced model accuracy by 30% over baseline models, achieving an impressive 97% accuracy on the test set.<div style="height: 10px;"></div>The model was rigorously validated with real-world data, showing a practical accuracy of 90%.<div style="height: 10px;"></div>An interactive UI was created with IPython widgets for real-time classification, demonstrating potential medical applications.<div style="height: 10px;"></div>This project not only highlights technical proficiency but also a commitment to leveraging technology for meaningful impact.<div style="height: 10px;"></div>For more details, explore the <a href="https://github.com/KAUSSHIK/BrainTumorClassification">GitHub repository</a> or view my resume.',
 		shortDescription:
-			'A Deep Learning model to classify brain tumors from 2D MRI scans comprising of over 3500 images.',
+			'Fine-tuned a deep learning model to classify brain tumors from 2D MRI scans comprising of over 3500 images. Verified predictions with real life data from hospiatls in India. <br><br><b>Role:</b> Machine Learning Engineer',
 		links: [{ to: 'https://github.com/KAUSSHIK/BrainTumorClassification', label: 'Classifier on Github' }],
 		logo: Assets.TensorFlow,
 		name: 'Brain Tumor Classification',
 		period: {
-			from: new Date()
+			from: new Date('2023-09-16'),
+			to: new Date('2023-10-17')
 		},
-		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
-		type: 'Website Template',
-		screenshots: [
-			{
-				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			}
-		]
+		skills: getSkills('Tf', 'numpy', 'py', 'keras'),
+		type: 'Machine Learning Project',
+		// screenshots: [
+		// 	{
+		// 		label: 'screen 1',
+		// 		src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+		// 	},
+		// 	{
+		// 		label: '2',
+		// 		src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+		// 	},
+		// 	{
+		// 		label: '3',
+		// 		src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		// 	},
+		// 	{
+		// 		label: '4',
+		// 		src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		// 	},
+		// 	{
+		// 		label: '5',
+		// 		src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		// 	},
+		// 	{
+		// 		label: '6',
+		// 		src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		// 	}
+		// ]
 	}
 ];
 
