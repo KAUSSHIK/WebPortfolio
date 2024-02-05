344f9ae213c582cf3845e50ba6387ed0aa1ce17c
KAUSSHIK <kausshikmanojkumar@gmail.com>
2024-02-05 11:57:35 -0600

    Remove commented out code for faulty experiences

--- src/lib/experiences.params.ts
+++ src/lib/experiences.params.ts
@@ -18,36 +18,36 @@ const MY_EXPERIENCES: Array<Experience> = [
 		logo: Assets.RTX,
 		shortDescription: 'At Collins Aerospace, I executed high-level and low-level tests within an Agile environment, significantly enhancing software reliability. My role involved translating requirements into structured ADA code, aligning with project objectives for flight management systems. I led the formulation and refinement of test cases, improving testing efficiency and mitigating security risks. Additionally, I played a key role in updating an old code base with newer calculations to improve the accuracy of flight management systems'
 	},
-	{
-		slug: 'software-freelance',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
-		color: '#ffffff',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'At Collins Aerospace, I executed high-level and low-level tests within an Agile environment, significantly enhancing software reliability. My role involved translating requirements into structured ADA code, aligning with project objectives for flight management systems. I led the formulation and refinement of test cases, improving testing efficiency and mitigating security risks. Additionally, I played a key role in updating an old code base with newer calculations to improve the accuracy of flight management systems'
-	},
-	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: '#ffffff',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: ''
-	}
+	// {
+	// 	slug: 'software-freelance',
+	// 	company: 'Self-employed',
+	// 	description: 'Creating awesome applications for customers.',
+	// 	contract: ContractType.Freelance,
+	// 	type: 'Software Development',
+	// 	location: 'Home',
+	// 	period: { from: new Date() },
+	// 	skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
+	// 	name: 'Freelancer',
+	// 	color: '#ffffff',
+	// 	links: [],
+	// 	logo: Assets.Unknown,
+	// 	shortDescription: 'At Collins Aerospace, I executed high-level and low-level tests within an Agile environment, significantly enhancing software reliability. My role involved translating requirements into structured ADA code, aligning with project objectives for flight management systems. I led the formulation and refinement of test cases, improving testing efficiency and mitigating security risks. Additionally, I played a key role in updating an old code base with newer calculations to improve the accuracy of flight management systems'
+	// },
+	// {
+	// 	slug: 'software-freelance-junior',
+	// 	company: 'Self-employed',
+	// 	description: 'Creating awesome applications for customers.',
+	// 	contract: ContractType.Freelance,
+	// 	type: 'Software Development',
+	// 	location: 'Home',
+	// 	period: { from: new Date(2022, 0, 1), to: new Date() },
+	// 	skills: getSkills('css', 'html', 'js'),
+	// 	name: 'Junior Freelancer',
+	// 	color: '#ffffff',
+	// 	links: [],
+	// 	logo: Assets.Unknown,
+	// 	shortDescription: ''
+	// }
 ];
 
 export default MY_EXPERIENCES;
